99df0e25b63ad3e9b0d378e243d29242042775ef
SHREYAS MAHAJAN <58950605+mahajan-Programming@users.noreply.github.com>
2024-01-21 22:57:18 +0530

    deployment correction

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-
-// const app =express();
-
-// app.use(express.static(__dirname + './dist/'));
-
-// app.get('/*',(req,res)=>
-// res.sendFile('index.html',{root:'dist/'}),
-// );
-
-// app.listen(process.env.PORT || 8080);
-
 const express = require('express');
 
 const path = require('path');
