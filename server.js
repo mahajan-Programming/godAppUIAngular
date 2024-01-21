8ecb4752dacc460314e3358620fb63739c3d29b6
SHREYAS MAHAJAN <58950605+mahajan-Programming@users.noreply.github.com>
2024-01-21 22:17:43 +0530

    deployment correction

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app =express();
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist/'));
 
 app.get('/*',(req,res)=>
-res.sendFile('index.html',{root:'dist/angular-heroku'}),
+res.sendFile('index.html',{root:'dist/'}),
 );
 
 app.listen(process.env.PORT || 8080);
