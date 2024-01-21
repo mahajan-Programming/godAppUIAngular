91c3b54e6e8ad39693eb0dfff315fa4e9acbc7f8
SHREYAS MAHAJAN <58950605+mahajan-Programming@users.noreply.github.com>
2024-01-21 23:13:26 +0530

    deployment correction

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/employee-management'));
 
-app.get('/*', function(req,res) {res.sendFile('/dist/employee-management/index.html');
+app.get('/*', function(req,res) {res.sendFile(__dirname + '/dist/employee-management/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
